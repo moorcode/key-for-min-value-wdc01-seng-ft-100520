5e125db6c736d237c85b04afae910f666ee801d8
Christina Poole <codenamedchris@gmail.com>
2020-09-19 21:39:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  output_name = ""
-  name_hash_array = name_hash.to_a
-  row_index = 0
-  while row_index < name_hash_array.count  do
-    if row_index[0].last < row_index.last
-        output_name = name_hash_array[row_index].first.to_s
-        row_index += 1
-    end
+  name_hash_array = name_hash.to_a #turn hash to AoA
+  inner_array_integers = []
+  inner_array = 0
+  while inner_array < name_hash_array.count  do
+    inner_array_integers << name_hash_array[inner_array].last #put AoA integers, 1th/last value in nested array,in separate array
+    inner_array += 1
+  end
+  inner_array_integers.reject do |number|
+    number.max(inner_array_integers.length - 1) #reject all highest numbers except one, leaving min in array
   end
   if name_hash.empty?
-    nil
-  else
-      name_hash_array[0]
+    nil    
   end
+  name_hash_array[name_hash_array.index(inner_array_integers.to_s) - 1]
 end

aaf21a032c2ae03146812361ddcb146998697622
Christina Poole <codenamedchris@gmail.com>
2020-09-19 21:50:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,9 +12,7 @@ def key_for_min_value(name_hash)
     inner_array_integers << name_hash_array[inner_array].last #put AoA integers, 1th/last value in nested array,in separate array
     inner_array += 1
   end
-  inner_array_integers.reject do |number|
-    number.max?(inner_array_integers.length - 1) #reject all highest numbers except one, leaving min in array
+  inner_array_integers.min_by do |number|
+    name_hash_array[number]
   end
-  name_hash_array[name_hash_array.index(inner_array_integers.to_s)]
-
 end

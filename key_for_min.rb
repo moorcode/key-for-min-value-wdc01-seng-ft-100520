0cef1acf8a9ae8aac0a86da93cdd635efd66fdf2
Christina Poole <codenamedchris@gmail.com>
2020-09-19 22:01:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,6 @@ def key_for_min_value(name_hash)
   name_hash_array = name_hash.to_a #turn hash to AoA
   inner_array_integers = []
   inner_array = 0
-  while inner_array < name_hash_array.count  do
-    inner_array_integers << name_hash_array[inner_array].last #put AoA integers, 1th/last value in nested array,in separate array
-    inner_array += 1
-  end
 
   if name_hash.empty?
     nil
